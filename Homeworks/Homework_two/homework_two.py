1115c78adc9210d30b119e0e1150492df379ce0d
Valery_Khramov <dr.hramoff2013@yandex.ru>
2024-03-19 19:34:21 +0300

    added: second homework

---
+++ Homeworks/Homework_two/homework_two.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, make_response, redirect, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('auth'))
+
+
+@app.route('/auth/', methods=['GET', 'POST'])
+def auth():
+    context = {'title': 'Авторизация'}
+    if request.method == 'POST':
+        username = request.form.get('auth_username')
+        email = request.form.get('auth_email')
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie('username', username)
+        response.set_cookie('email', email)
+        return response
+    response = make_response(render_template('auth.html', **context))
+    return response
+
+
+@app.route('/logout/')
+def logout():
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('username', '', expires=0)
+    response.set_cookie('email', '', expires=0)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
